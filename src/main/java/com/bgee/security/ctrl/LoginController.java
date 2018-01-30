3d2fdca141c1b0022137684684f7625cd6073648
bgee0915 <bgee0915@outlook.com>
2018-01-30 23:39:29 +0800

    login

--- src/main/java/com/bgee/security/ctrl/LoginController.java
+++ src/main/java/com/bgee/security/ctrl/LoginController.java
@@ -2,6 +2,7 @@ package com.bgee.security.ctrl;
 
 import com.alibaba.druid.support.logging.Log;
 import com.alibaba.druid.support.logging.LogFactory;
+import com.bgee.security.entity.Account;
 import com.bgee.security.entity.R;
 import com.bgee.security.service.AccountService;
 import org.springframework.stereotype.Controller;
@@ -11,20 +12,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 
 @Controller
-@RequestMapping("/account")
-public class AccountController {
+@RequestMapping("/login")
+public class LoginController {
     private Log log = LogFactory.getLog(this.getClass());
 
     @Resource
     private AccountService accountService;
 
     @ResponseBody
-    @RequestMapping("/get")
-    public R get(Integer id){
+    @RequestMapping("/login")
+    public R login(String account, String pass){
         try {
-            return new R(1,accountService.get(id),true);
+            Account acct = accountService.getAccountPass(account,pass);
+            if(acct == null){
+                return new R(0,"account null", false);
+            } else {
+                return new R(1,acct,true);
+            }
         } catch (Exception e){
-            log.error("AccountController, get , id=" + id +",  e=" + e.getMessage());
+            log.error("LoginController, login, account=" + account + ", pass=" + pass + ",  e=" , e);
             return new R(0,e.getMessage(),false);
         }
     }
